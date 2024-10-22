eccc2ccfee2ba3ad53ba94148c623463db67cf4a
fadesyphe <pivkin.jura2017@gmail.com>
2024-10-22 10:59:31 +0600

    Created TheAdvantagesSecondaryList component

--- src/components/TheAdvantages.vue
+++ src/components/TheAdvantages.vue
@@ -2,66 +2,22 @@
   <section class="section section--advantages mt-9">
     <TheAdvantagesMain />
     <TheAdvantagesFirstList />
-    <div class="advantages-list advantages-list--green-back">
-      <div class="container">
-        <div class="advantages-text-container">
-          <div class="advantages-list-item">
-            <h3 class="advantages-item-title">
-              <span>Доступные цены:</span>
-            </h3>
-            <p class="advantages-item-description">
-              Мы предлагаем конкурентоспособные <br />
-              цены без ущерба для качества.
-            </p>
-          </div>
-          <div class="advantages-list-item">
-            <h3 class="advantages-item-title">
-              <span>Опыт и профессионализм:</span>
-            </h3>
-            <p class="advantages-item-description">
-              Наша небольшая команда состоит из <br />
-              опытных специалистов, которые <br />
-              гарантируют отличный результат.
-            </p>
-          </div>
-          <div class="advantages-list-item advantages-list-item--back">
-            <h3 class="advantages-item-title">
-              <span>Уникальные</span>
-            </h3>
-            <br />
-            <h3 class="advantages-item-title">современные дизайны</h3>
-            <p class="advantages-item-description">
-              Мы предлагаем стильные, <br />
-              современные решения для детской одежды.
-            </p>
-          </div>
-          <div class="advantages-list-item">
-            <h3 class="advantages-item-title">
-              <span>Доверие и надёжность</span>
-            </h3>
-            <p class="advantages-item-description">
-              Мы строим долгосрочные отношения <br />
-              с нашими клиентами, основанные на честности,<br />прозрачности и
-              выполнении всех обязательств.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="advantages-list-background"></div>
-    </div>
+    <TheAdvantagesSecondaryList />
   </section>
 </template>
 
 <script>
 import BaseButton from './BaseButton.vue'
-import TheAdvantagesMain from "./TheAdvantagesMain.vue"
-import TheAdvantagesFirstList from "./TheAdvantagesFirstList.vue"
+import TheAdvantagesMain from './TheAdvantagesMain.vue'
+import TheAdvantagesFirstList from './TheAdvantagesFirstList.vue'
+import TheAdvantagesSecondaryList from './TheAdvantagesSecondaryList.vue'
 
 export default {
   components: {
     BaseButton,
     TheAdvantagesMain,
     TheAdvantagesFirstList,
+    TheAdvantagesSecondaryList
   },
 }
 </script>
---
+++ src/components/TheAdvantagesSecondaryList.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="w-full max-w-[1100px] my-8 text-black font-bold flex items-end">
+    <div class="w-full max-w-[207px] relative z-20">
+      <div class="bg-green pl-8 pt-2 pb-4">
+        <div class="mb-7">
+          <div class="relative z-20">
+            <h3 class="text-[18px] leading-none relative z-20">
+              Доступные цены:
+            </h3>
+            <img
+              class="absolute z-[19] w-[118px] h-[8px] left-0 top-4"
+              src="../assets/images/title-decoration.svg"
+              alt="Подчеркивание заголовка"
+            />
+          </div>
+          <p class="text-[9px] leading-none mt-[6px]">
+            Мы предлагаем конкурентоспособные <br />
+            цены без ущерба для качества.
+          </p>
+        </div>
+        <div class="mb-7">
+          <div class="relative z-20">
+            <h3 class="text-[18px] leading-none relative z-20">
+              Опыт и профессионализм:
+            </h3>
+            <img
+              class="absolute z-[19] w-[118px] h-[8px] left-0 top-4"
+              src="../assets/images/title-decoration.svg"
+              alt="Подчеркивание заголовка"
+            />
+          </div>
+          <p class="text-[9px] leading-none mt-[6px]">
+            Наша небольшая команда состоит из <br />
+            опытных специалистов, которые <br />
+            гарантируют отличный результат.
+          </p>
+        </div>
+        <div class="">
+          <div class="relative z-20">
+            <h3 class="text-[18px] leading-none relative z-20">
+              Уникальные современные дизайны:
+            </h3>
+            <img
+              class="absolute z-[19] w-[118px] h-[8px] left-0 top-4"
+              src="../assets/images/title-decoration.svg"
+              alt="Подчеркивание заголовка"
+            />
+          </div>
+          <p class="text-[9px] leading-none mt-[6px]">
+            Мы предлагаем стильные, <br />
+            современные решения для детской одежды.
+          </p>
+        </div>
+      </div>
+      <div class="pl-8 pt-3 pb-4 bg-orange">
+        <div class="relative z-20">
+          <h3 class="text-[18px] leading-none relative z-20">
+            Доверие и надежность:
+          </h3>
+          <img
+            class="absolute z-[19] w-[118px] h-[8px] left-0 top-4"
+            src="../assets/images/title-decoration.svg"
+            alt="Подчеркивание заголовка"
+          />
+        </div>
+        <p class="text-[9px] leading-none mt-[6px]">
+          Мы строим долгосрочные отношения <br />
+          с нашими клиентами, основанные на <br />честности, прозрачности и
+          выполнении всех обязательств.
+        </p>
+      </div>
+    </div>
+    <div>
+      <img class="w-full min-w-[180px] h-auto -ml-3 relative z-10" src="../assets/images/Ресурс 5.svg" alt="">
+    </div>
+  </div>
+</template>
+
+<script></script>
