add55afac45c754497cdf7d11769b48c614c6837
fadesyphe <pivkin.jura2017@gmail.com>
2024-10-23 15:56:01 +0600

    Created TheSocial components

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -14,37 +14,19 @@
       <BaseButton class="flex mx-auto text-center my-4">
         Свяжитесь с нами
       </BaseButton>
-      <div class="flex items-center justify-center gap-x-3">
-          <a v-for="item in list" :href="item.link" :key="item.name" target="_blank">
-            <BaseIcon
-              :name="item.name"
-              :viewBox="item.viewBox"
-              class="w-5 h-5 fill-white hover:fill-orange duration-500 ease-out"
-            />
-          </a>
-      </div>
+      <TheSocial />
     </div>
   </section>
 </template>
 
 <script>
-import BaseIcon from './BaseIcon.vue'
 import BaseButton from './BaseButton.vue'
+import TheSocial from './TheSocial.vue'
 
 export default {
   components: {
-    BaseIcon,
     BaseButton,
+    TheSocial,
   },
-
-  data() {
-    return {
-      list: [
-        { link: '#', name: 'vk', viewBox: '0 0 512 512' },
-        { link: '#', name: 'whatsapp', viewBox: '0 0 512 512' },
-        { link: '#', name: 'telegram', viewBox: '0 0 100 100' },
-      ]
-    }
-  }
 }
 </script>
---
+++ src/components/TheSocial.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-center justify-center gap-x-3">
+    <a v-for="item in list" :href="item.link" :key="item.name" target="_blank">
+      <BaseIcon
+        :name="item.name"
+        :viewBox="item.viewBox"
+        class="w-5 h-5 fill-white hover:fill-orange duration-500 ease-out"
+      />
+    </a>
+  </div>
+</template>
+
+<script>
+import BaseIcon from "./BaseIcon.vue"
+
+export default {
+  components: {
+    BaseIcon,
+  },
+  
+  data() {
+    return {
+      list: [
+        { link: '#', name: 'vk', viewBox: '0 0 512 512' },
+        { link: '#', name: 'whatsapp', viewBox: '0 0 512 512' },
+        { link: '#', name: 'telegram', viewBox: '0 0 100 100' },
+      ]
+    }
+  }
+}
+</script>
